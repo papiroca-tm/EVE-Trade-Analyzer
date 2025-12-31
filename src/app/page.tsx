070e1cdc36e952cee0389d5c0c4746fde7c08aab
Вячеслав Даньшин <Danshin.workspace@gmail.com>
2025-12-31 14:52:37 +0000

    теперь выделенную секцию где параметры рынка и стакан сделай не 20% по о

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,8 +28,8 @@ export default function Home() {
           </h1>
         </div>
       </header>
-      <main className="grid flex-1 grid-cols-5 gap-2 p-2 md:gap-4 md:p-4">
-        <div className="col-span-1">
+      <main className="grid flex-1 grid-cols-10 gap-2 p-2 md:gap-4 md:p-4">
+        <div className="col-span-3">
             <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
                 <div className="col-span-1">
                     <div className="sticky top-16 flex flex-col gap-2">
@@ -47,7 +47,7 @@ export default function Home() {
                 </div>
             </div>
         </div>
-        <div className="col-span-4">
+        <div className="col-span-7">
           <ResultsDisplay state={state} />
         </div>
       </main>
